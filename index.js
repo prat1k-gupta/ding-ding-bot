137b666e8bbaaaf844d13cf98a5f74c100503b6e
prat1k-gupta <pratikgupta9773@gmail.com>
2022-07-22 20:52:39 +0530

    updated code

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const token = require('./config.json')
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
-
+require("dotenv").config()
+const token = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID; 
+const GUILD_ID = process.env.GUILD_ID;
 const commands = [
   {
     name: 'ding',
-    description: 'Replies with Pong!',
-  },
+    description: 'it replies you with a beautiful message',
+  }
 ];
 
-const rest = new REST({ version: '10' }).setToken(token.token);
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    await rest.put(Routes.applicationGuildCommands("1000026083615330385", "966764890419249223"), { body: commands });
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
 
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
@@ -37,4 +39,4 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
-client.login(token.token);
+client.login(token);
